dc2ec465db27818a19697570812c370ae0b8dac2
jorge88dev <jgarcia@gvam.es>
2022-06-08 12:13:34 +0200

    Combined field Query, add rest of logic.

--- src/Query/FullText/CombinedFieldsQuery.php
+++ src/Query/FullText/CombinedFieldsQuery.php
@@ -11,6 +11,9 @@ class CombinedFieldsQuery extends Query
 {
     private const IDENTIFIER = 'combined_fields';
 
+    public const ZERO_TERMS_QUERY_NONE = 'none';
+    public const ZERO_TERMS_QUERY_ALL = 'all';
+
     public const OR = 'or';
     public const AND = 'and';
 
@@ -30,6 +33,21 @@ class CombinedFieldsQuery extends Query
     private string $operator;
 
     /**
+     * @var string
+     */
+    private string $zeroTermsQuery;
+
+    /**
+     * @var bool
+     */
+    private bool $autoGenerateSynonymsPhraseQuery;
+
+    /**
+     * @var string
+     */
+    private string $minimumShouldMatch;
+
+    /**
      * @param array $fields
      * @param mixed $value
      * @throws InvalidParameterException
@@ -43,18 +61,25 @@ class CombinedFieldsQuery extends Query
         });
         $this->fields = $fields;
         $this->value = $value;
-        $this->operator = self::OR;
     }
 
+    /**
+     * @return array[]
+     */
     public function normalize(): array
     {
-        return [
+        $normalize =  [
             self::IDENTIFIER => [
                 'query' => $this->value,
-                'fields' => $this->fields,
-                'operator' => $this->operator
+                'fields' => $this->fields
             ]
         ];
+        !isset($this->operator) || ($normalize['operator'] = $this->operator);
+        !isset($this->zeroTermsQuery) || ($normalize['zero_terms_query'] = $this->zeroTermsQuery);
+        !isset($this->autoGenerateSynonymsPhraseQuery) || ($normalize['auto_generate_synonyms_phrase_query'] = $this->autoGenerateSynonymsPhraseQuery);
+        !isset($this->minimumShouldMatch) || ($normalize['minimum_should_match'] = $this->minimumShouldMatch);
+
+        return $normalize;
     }
 
     /**
@@ -68,5 +93,45 @@ class CombinedFieldsQuery extends Query
             throw new InvalidParameterException('Operators allowed {and, or}');
         }
         $this->operator = $operator;
+    }
+
+    /**
+     * @param string $zeroTermsQuery
+     * @return $this
+     * @throws InvalidParameterException
+     */
+    public function setZeroTermsQuery(string $zeroTermsQuery): self
+    {
+        if ($zeroTermsQuery !== self::ZERO_TERMS_QUERY_NONE || $zeroTermsQuery !== self::ZERO_TERMS_QUERY_ALL) {
+            throw new InvalidParameterException('zero_terms_query allowed {all, none}');
+        }
+        $this->zeroTermsQuery = $zeroTermsQuery;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $autoGenerateSynonymsPhraseQuery
+     * @return $this
+     */
+    public function setAutoGenerateSynonymsPhraseQuery(bool $autoGenerateSynonymsPhraseQuery): self
+    {
+        $this->autoGenerateSynonymsPhraseQuery = $autoGenerateSynonymsPhraseQuery;
+
+        return $this;
+    }
+
+    /**
+     * @param string $minimumShouldMatch
+     * @return $this
+     */
+    public function setMinimumShouldMatch(string $minimumShouldMatch): self
+    {
+        /**
+         * TODO: Validate
+         */
+        $this->minimumShouldMatch = $minimumShouldMatch;
+
+        return $this;
     }
 }
